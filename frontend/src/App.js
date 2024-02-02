f6dca7f5ea3e4b8786314dd5dcd0fd8383b2d8ca
Aditya <adityabharti.aktu2020@gmail.com>
2024-02-02 16:54:27 +0530

    4rth aommit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,10 @@
 function App() {
-  return <div className="App">Aditya and faizan</div>;
+  return (
+    <div className="App">
+      <h2>The Git Practive Portal</h2>
+      <p>This the the portal for git practice</p>
+    </div>
+  );
 }
 
 export default App;
