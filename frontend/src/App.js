83ef77bcc15a0be21c487663901cdc752eb4f37c
Aditya <adityabharti.aktu2020@gmail.com>
2024-02-02 17:37:07 +0530

    5th commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ function App() {
     <div className="App">
       <h2>The Git Practive Portal</h2>
       <p>This the the portal for git practice</p>
+      <p>Trying to next revert </p>
     </div>
   );
 }
